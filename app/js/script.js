1cecbf4a0e6df45175075dd2ae030153c18cf5cb
brandon <bmd84@georgetown.edu>
2021-06-17 15:45:36 -1000

    use .value to get value of selected tag

--- app/js/script.js
+++ app/js/script.js
@@ -214,9 +214,7 @@ mymap.on('zoomend', function(e) {
 var locationParams, seasonParams, timescaleParams;
 function downloadSubmit() {
   //get checked values
-  locationParams = $("#dDown").find(':selected')[0];
-  console.log(testVar);
-  //locationParams = document.querySelectorAll('.locCheck:selected'); //nodelist
+  locationParams = $("#dDown").find(':selected')[0].value;
   seasonParams = document.querySelectorAll('.seasonCheck:checked');
   timescaleParams = document.querySelectorAll('.timeCheck:checked');
   if (locationParams == null || seasonParams == null || timescaleParams == null 
