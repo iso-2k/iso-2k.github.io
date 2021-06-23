f664f3f30fcd4596a696c0fcce117423f9cb8d78
brandon <bmd84@georgetown.edu>
2021-06-23 08:02:02 -1000

    clean up html and add transparent background to button

--- app/js/script.js
+++ app/js/script.js
@@ -265,8 +265,6 @@ function downloadSubmit() {
 
 }
 
-
-
 $(document).ready(function () {
   // add bootstrap table styles to pandoc tables
   $('tr.header').parent('thead').parent('table').addClass('table table-condensed');
