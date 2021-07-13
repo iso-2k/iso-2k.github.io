fb388de86c472848f3186a9c932f07ee1facc9f3
brandon <bmd84@georgetown.edu>
2021-07-13 15:23:27 -0700

    need to initialize dropdowns in js

---
+++ app/js/gui_script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() { //initialize select2 on dropdown
+    $('.js-example-placeholder-single').select2({
+      placeholder: "Proxy Location",
+      allowClear: true
+    });
+    $('.js-example-placeholder-single[name="timescales[]"]').select2({
+      placeholder: "Timescale",
+      allowClear: false
+    });
+    $('.js-example-placeholder-single[name="seasonality[]"]').select2({
+      placeholder: "Seasonality",
+      allowClear: false
+    });
+    $('.js-example-placeholder-single[name="mode_list[]"]').select2({
+        placeholder: "Modes of Variability",
+        allowClear: false
+      });
+  });
