527a70ea589d79732570dfc4f805c1e5f56b4841
brandon <bmd84@georgetown.edu>
2021-07-14 13:44:24 -0700

    2 diff potential solutions to limit dec places

--- app/js/gui_script.js
+++ app/js/gui_script.js
@@ -13,7 +13,15 @@ $(document).ready(function() { //initialize select2 on dropdown
       });
   });
 
+$('#latitude').blur(function() {
+  this.value = parseFloat(this.value).toFixed(4);
+});
 
+
+function setDecimal(event) {
+  this.value = parseFloat(this.value).toFixed(4);
+}
+  
   //JS FOR FORM SUBMISSION
 var inputLat, inputLon, inputTscale, inputSeas, inputModes;
 function downloadSubmit() {
